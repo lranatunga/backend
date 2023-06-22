9ea8087f8bf5682387c42b94773cf25763e555c8
lranatunga <rmlik.ranatunga@gmail.com>
2023-06-22 18:38:20 +0100

    mongoose-intro

--- mongoose-intro/server/src/index.js
+++ mongoose-intro/server/src/index.js
@@ -1,7 +1,13 @@
 import  express from 'express'
 import cors from 'cors'
+import dotenv from "dotenv"
+import dbConnect from './config/db.js'
 
+
+dotenv.config()
+dbConnect()
 const app = express()
-app.use(cors())
 
+
+app.use(cors())
 app.listen(5000, () => console.log('Server is up and running at port 5000'))
